e2471ca7e7a831c65a043a3647aa7ce79bc4ffd7
Ars Vladimirov <grokhael@gmail.com>
2023-07-20 17:09:44 +0600

    update custom data

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,8 +1,8 @@
-**MinneApple: A benchmark dataset for apple detection and segmentation** is a dataset for instance segmentation, object detection, and semantic segmentation tasks. It is used in the agriculture industry.
+**MinneApple: A benchmark dataset for apple detection and segmentation** is a dataset for instance segmentation, object detection, and semantic segmentation tasks. It is used in the agricultural industry. 
 
-The **detection.tar.gz** dataset consists of 1001 images with 23065 labelled objects belonging to 1 single class (*apple*).
+The dataset consists of 1001 images with 23065 labeled objects belonging to 1 single class (*apple*).
 
-Each image in the MinneApple dataset has pixel-level instance segmentation annotations. Due to the nature of the instance segmentation task, it can be automatically transformed into semantic segmentation (only one mask for every class) or object detection (bounding boxes for every object) tasks. There are 331 (33% of the total) unlabeled images (i.e. without annotations). There are 2 splits in the dataset: *Test* (331 images) and *Train* (670 images). The dataset was released in 2019 by the [University of Minnesota Robotic Sensor Network Laboratory](https://rsn.umn.edu/).
+Images in the MinneApple dataset have pixel-level instance segmentation annotations. Due to the nature of the instance segmentation task, it can be automatically transformed into a semantic segmentation (only one mask for every class) or object detection (bounding boxes for every object) tasks. There are 331 (33% of the total) unlabeled images (i.e. without annotations). There are 2 splits in the dataset: *Test* (331 images) and *Train* (670 images). The dataset was released in 2019 by the [University of Minnesota Robotic Sensor Network Laboratory](https://rsn.umn.edu/).
 
 Here is the visualized example grid with annotations:
 
--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,14 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
@@ -12,7 +20,9 @@ PROJECT_NAME_FULL: str = "MinneApple: A benchmark dataset for apple detection an
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC_BY_NC_SA_3_0_US()
-INDUSTRIES: List[Industry] = [Industry.Agriculture()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Agricultural()]
+CATEGORY: Category = Category.Agriculture()
+
 CV_TASKS: List[CVTask] = [
     CVTask.InstanceSegmentation(),
     CVTask.ObjectDetection(),
@@ -20,7 +30,10 @@ CV_TASKS: List[CVTask] = [
 ]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.InstanceSegmentation()]
 
-RELEASE_YEAR: int = 2019
+RELEASE_DATE: Optional[str] = "2019-09-13"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
 HOMEPAGE_URL: str = "https://conservancy.umn.edu/handle/11299/206575"
 # e.g. "https://some.com/dataset/homepage"
 
@@ -46,10 +59,16 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = {"apple": [245, 166, 35]}
 
 PAPER: Optional[str] = "https://ieeexplore.ieee.org/document/8954630"
 CITATION_URL: Optional[str] = "https://conservancy.umn.edu/handle/11299/206575"
+AUTHORS: Optional[List[str]] = [
+    "Nicolai Häni",
+    "Pravakar Roy",
+    "Volkan Isler",
+]
 ORGANIZATION_NAME: Optional[
     Union[str, List[str]]
 ] = "University of Minnesota Robotic Sensor Network Laboratory"
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://rsn.umn.edu/"
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -64,11 +83,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
-        "project_name_full": PROJECT_NAME_FULL,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -76,15 +99,20 @@ def get_settings():
         "preview_image_id": PREVIEW_IMAGE_ID,
         "github_url": GITHUB_URL,
     }
+
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
+    settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
