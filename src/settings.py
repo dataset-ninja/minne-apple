a73e3d3416acaf11b927b1b35ecfff39a5c06b78
Ars Vladimirov <grokhael@gmail.com>
2023-08-02 19:04:42 +0600

    CVTask

--- src/settings.py
+++ src/settings.py
@@ -25,8 +25,8 @@ CATEGORY: Category = Category.Agriculture()
 
 CV_TASKS: List[CVTask] = [
     CVTask.InstanceSegmentation(),
-    CVTask.ObjectDetection(),
     CVTask.SemanticSegmentation(),
+    CVTask.ObjectDetection(),
 ]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.InstanceSegmentation()]
 
